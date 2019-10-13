30e714dca5b03eb79c77ea9296e1b59629461f64
James <Jamespr.0312@gmail.com>
2019-10-12 19:51:53 -0500

    Complete rebuild
    Got rid of class - using dict's instead. Formatting is completed
    
    TODO:
    Add in sorting through data to set aside orders that have a irregular order status

--- CentiroCode.py
+++ CentiroCode.py
@@ -1,22 +1,18 @@
-class Customer:
-  def makeString(self):
-    print(self.orderNo + " " + self.name + "   " + self.status)
- 
 inFile = open("inFile.txt","r+")
 outFile  = open("outPutFile.txt", "w+")
  
 customerList = []
 
 for line in inFile:
-  tempString = line.split(";")
- 
-  foo = Customer()
-  foo.orderNo = tempString[0]
-  foo.name = tempString[1]
-  foo.status = tempString[11]
- 
-  customerList.append(foo)
- 
-for foo in customerList:
-  foo.makeString()
- 
+    tempString = line.split(";")
+    nameSplit = tempString[1].split(" ")
+
+    foo = dict(orderNo = tempString[0], fullName = tempString[1],
+    lastName = nameSplit[1], status = tempString[11])
+
+    customerList.append(foo)
+
+printList = sorted(customerList, key = lambda i: i['lastName'])   
+
+for foo in printList:
+    print("{f[orderNo]} {f[fullName]:25} {f[status]}".format(f = foo))
