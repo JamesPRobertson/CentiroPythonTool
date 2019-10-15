cf41776cf3b4c2fdb4a264ee5122b68f7b735b87
James <Jamespr.0312@gmail.com>
2019-10-15 00:11:05 -0500

    Major code change - see extended description
    - inFile now uses glob.glob to locate existing .csv within folder
    - Added support for determining orders with an irregular status and separating them
    - The online Python tool (repl.it) cannot write to output files correctly so output statements must be changed to print() for it to work

--- CentiroCode.py
+++ CentiroCode.py
@@ -1,7 +1,13 @@
-inFile = open("inFile.txt","r+")
+import glob
+
+dirList = glob.glob('*.csv')
+
+inFile = open(dirList[0],"r+")
 outFile  = open("outputFile.txt", "w+")
 
 customerList = []
+standardList = []
+irregularList = []
 
 for line in inFile:
     tempString = line.split(";")
@@ -12,7 +18,21 @@ for line in inFile:
 
     customerList.append(foo)
 
+for entry in customerList:
+    if entry['status'] != "SP_CONTACTED sent to iSell" and entry['status'] != "Received at Hub":
+        irregularList.append(entry)
+    else:
+        standardList.append(entry)
+
 printList = sorted(customerList, key = lambda i: i['lastName']) 
 
-for entry in printList:
+outFile.write("*********************************************************************\n")
+
+for entry in standardList:
+    outFile.write("[ ] {f[orderNo]} {f[fullName]:28.26} {f[status]:.26}".format(f = entry) + "\n")
+
+outFile.write("\n*********************************************************************")
+outFile.write("\nThese orders need attention:\n")
+
+for entry in irregularList:
     outFile.write("[ ] {f[orderNo]} {f[fullName]:28.26} {f[status]:.26}".format(f = entry) + "\n")
