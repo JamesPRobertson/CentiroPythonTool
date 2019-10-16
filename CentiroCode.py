651fbedd5bb8b4084814014d787886801439a2b1
James <Jamespr.0312@gmail.com>
2019-10-15 19:58:16 -0500

    Some changes
    Reordered irregularity detector, added exception to be raised if glob.glob cannot locate a .csv

--- CentiroCode.py
+++ CentiroCode.py
@@ -2,10 +2,12 @@ import glob
 
 dirList = glob.glob('*.csv')
 
+if len(dirList) == 0:
+    raise RuntimeError("No matching files found: *.csv")
+
 inFile = open(dirList[0],"r+")
 outFile  = open("outputFile.txt", "w+")
 
-customerList = []
 standardList = []
 irregularList = []
 
@@ -13,18 +15,16 @@ for line in inFile:
     tempString = line.split(";")
     nameSplit = tempString[1].split(" ")
 
-    foo = dict(orderNo = tempString[0], fullName = tempString[1],
+    tempDict = dict(orderNo = tempString[0], fullName = tempString[1],
     lastName = nameSplit[1], status = tempString[11])
 
-    customerList.append(foo)
-
-for entry in customerList:
-    if entry['status'] != "SP_CONTACTED sent to iSell" and entry['status'] != "Received at Hub":
-        irregularList.append(entry)
+    if tempDict['status'] == "SP_CONTACTED sent to iSell" or tempDict['status'] == "Received at Hub":
+        standardList.append(tempDict)
     else:
-        standardList.append(entry)
+        irregularList.append(tempDict)
 
-printList = sorted(customerList, key = lambda i: i['lastName']) 
+standardList = sorted(standardList, key = lambda i: i['lastName'])
+irregularList = sorted(irregularList, key = lambda i: i['lastName'])
 
 outFile.write("*********************************************************************\n")
 
@@ -32,7 +32,7 @@ for entry in standardList:
     outFile.write("[ ] {f[orderNo]} {f[fullName]:28.26} {f[status]:.26}".format(f = entry) + "\n")
 
 outFile.write("\n*********************************************************************")
-outFile.write("\nThese orders need attention:\n")
+outFile.write("\nThese orders need attention:\n\n")
 
 for entry in irregularList:
     outFile.write("[ ] {f[orderNo]} {f[fullName]:28.26} {f[status]:.26}".format(f = entry) + "\n")
